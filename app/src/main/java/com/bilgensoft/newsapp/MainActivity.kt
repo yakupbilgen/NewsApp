139d3d9aac0b950b5feb5d2829dfcd3f0643e327
yakupbilgen <yakupbilgen92@gmail.com>
2021-09-09 20:28:49 +0300

    fragment add

--- app/src/main/java/com/bilgensoft/newsapp/MainActivity.kt
+++ app/src/main/java/com/bilgensoft/newsapp/MainActivity.kt
@@ -2,11 +2,19 @@ package com.bilgensoft.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+    val fragmentManager= super.getSupportFragmentManager()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.supportActionBar?.hide()
     }
+
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        //fragmentTransaction.replace(R.id.frmFirst, fragment)
+        //fragmentTransaction.commit()
+
 }
